d0454ce53287701543459613fc9a8f69de360b94
mati <matiasmartinmercadoc3@gmail.com>
2024-02-02 01:15:48 -0300

    client subscription setup phase 1

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('client-dashboard', views.client_dashboard, name="client-dashboard")
+    path('client-dashboard', views.client_dashboard, name="client-dashboard"),
+    path('browse-articles', views.browse_articles, name="browse-articles"),
+    path('subscriptionl-locked', views.subscription_locked, name="subscriptionl-locked"),
 ]
--- client/views.py
+++ client/views.py
@@ -1,8 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from writer.models import Article
+from . models import Subscription
+
+
+
 
 
 @login_required(login_url='my_login')
 def client_dashboard(request):
     
-    return render(request, 'client/client-dashboard.html')
+    try:
+        subsDetails = Subscription.objects.get(user=request.user)
+        subscription_plan = subsDetails.subscription_plan
+        context = {'SubPlan':subscription_plan}
+        return render(request, 'client/client-dashboard.html', context)
+        
+    except:
+        subscription_plan = "None"
+        context = {'SubPlan': subscription_plan}
+        return render(request, 'client/client-dashboard.html', context)
+    
+
+
+
+#######
+#######
+#######
+
+
+
+@login_required(login_url='my_login')
+def browse_articles(request):
+    
+    #que el usuario conectado que va a ver los articulos sea uno que este subscripto
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+        
+    except:
+        return render(request, 'client/subscription-locked.html')
+    
+    current_subscription_plan = subDetails.subscription_plan
+    
+    if current_subscription_plan == 'Standard':
+        articles = Article.objects.all().filter(is_premium=False)
+        
+    elif current_subscription_plan == 'Premium':
+        articles = Article.objects.all()
+        
+    context = {'AllClientsArticles':articles}
+    return render(request, 'client/browse-articles.html', context)
+
+
+
+
+##########
+##########
+##########
+
+
+
+
+@login_required(login_url='my_login')
+def subscription_locked(request):
+    
+    return render(request,'client/subscription-locked.html')
